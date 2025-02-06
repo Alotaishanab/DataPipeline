a3e5a36fd542cbc50632d6ddbea61646de4b3301
abdullah alotaishan <abdullah.alotaishan@kcl.ac.uk>
2025-02-06 13:29:51 +0000

    adjust the output

--- terraform/workers/outputs.tf
+++ terraform/workers/outputs.tf
@@ -2,6 +2,8 @@ output "worker_inventory" {
   description = "Mapping of worker host names to their IP addresses"
   value = {
     for idx, ip in [for w in harvester_virtualmachine.worker : w.network_interface[0].ip_address] :
-    "worker${idx + 1}" => { ansible_host = ip }
+    "worker${idx + 1}" => {
+      ansible_host = ip
+    }
   }
 }
