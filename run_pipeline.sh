6fab23abbda5280d90e1deee3f8512f8a98eff07
abdullah alotaishan <abdullah.alotaishan@kcl.ac.uk>
2025-03-14 21:04:22 +0000

    modify the shell script

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e
 
+# Change directory to the ansible folder to ensure ansible.cfg is used
+cd ansible
+
 echo "Running setup phase..."
-ansible-playbook ansible/playbooks/setup_phase.yml
+ansible-playbook playbooks/setup_phase.yml
 echo "Setup phase completed."
 
 echo "Running master pipeline..."
-ansible-playbook ansible/playbooks/master_pipeline.yml
+ansible-playbook playbooks/master_pipeline.yml
 echo "Master pipeline completed."
